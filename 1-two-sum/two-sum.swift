f067575965ae9955eaebe732050d22625ad02311
Dexter Jones <132309726+XA1626@users.noreply.github.com>
2025-09-26 12:00:44 +1200

    Time: 0 ms (100.00%) | Memory: 19.5 MB (86.12%) - LeetSync

--- 1-two-sum/two-sum.swift
+++ 1-two-sum/two-sum.swift
@@ -1,19 +1,18 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var numDict = [Int: Int]()  // value -> index
+        var numToIndex = [Int: Int]()
         
         for (index, num) in nums.enumerated() {
             let complement = target - num
             
-            // Check if complement exists in our dictionary
-            if let complementIndex = numDict[complement] {
+            if let complementIndex = numToIndex[complement] {
                 return [complementIndex, index]
             }
             
-            // Store current number with its index
-            numDict[num] = index
+            numToIndex[num] = index
         }
         
-        return []  // No solution found (though problem guarantees one exists)
+        // Since problem guarantees exactly one solution, we'll always return above
+        return []
     }
 }
