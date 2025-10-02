2868715cae51744f98429ec6cf9e8e428be907aa
Dexter Jones <132309726+XA1626@users.noreply.github.com>
2025-10-02 19:03:35 +1300

    Time: 1 ms (63.58%) | Memory: 212.2 MB (68.54%) - LeetSync

--- 1-two-sum/two-sum.rb
+++ 1-two-sum/two-sum.rb
@@ -2,18 +2,17 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-    num_to_index = {}
+    hash = {}
     
     nums.each_with_index do |num, index|
         complement = target - num
         
-        if num_to_index.key?(complement)
-            return [num_to_index[complement], index]
+        if hash.key?(complement)
+            return [hash[complement], index]
         end
         
-        num_to_index[num] = index
+        hash[num] = index
     end
     
-    # Since the problem guarantees exactly one solution, we'll always return above
     []
 end
