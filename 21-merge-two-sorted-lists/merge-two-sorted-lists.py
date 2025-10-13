22a7fef8a6e96ed7f1e35d33900b8e73fa9e2a0a
Dexter Jones <132309726+XA1626@users.noreply.github.com>
2025-10-13 21:48:21 +1300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (97.48%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,15 +1,30 @@
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        cur = dummy = ListNode()
-        while list1 and list2:               
-            if list1.val < list2.val:
-                cur.next = list1
-                list1, cur = list1.next, list1
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        # Create a dummy node to simplify the code
+        dummy = ListNode()
+        current = dummy
+        
+        # Traverse both lists and compare nodes
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
             else:
-                cur.next = list2
-                list2, cur = list2.next, list2
-                
-        if list1 or list2:
-            cur.next = list1 if list1 else list2
-            
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        
+        # Attach the remaining nodes from either list
+        current.next = list1 if list1 else list2
+        
         return dummy.next
