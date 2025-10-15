9c264414a7c207bcce4a4f5a19be6b86f6965565
Dexter Jones <132309726+XA1626@users.noreply.github.com>
2025-10-15 19:29:06 +1300

    Time: 0 ms (100.00%) | Memory: 19.8 MB (46.61%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func isPalindrome(_ x: Int) -> Bool {
+        // Negative numbers are not palindromes
+        if x < 0 { return false }
+        
+        // Numbers ending with 0 (except 0 itself) are not palindromes
+        if x != 0 && x % 10 == 0 { return false }
+        
+        var num = x
+        var reversed = 0
+        
+        // Reverse only half of the number
+        while num > reversed {
+            reversed = reversed * 10 + num % 10
+            num /= 10
+        }
+        
+        // For even digits: num == reversed
+        // For odd digits: num == reversed/10 (middle digit doesn't matter)
+        return num == reversed || num == reversed / 10
+    }
+}
